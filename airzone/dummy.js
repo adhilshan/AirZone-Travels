ee60cd118aa33e15f165589be3e91686ebd79b35
Adhil shan <adhilshanrahman@gmail.com>
2024-12-08 19:59:09 +0530

    v3

---
+++ airzone/dummy.js
@@ -0,0 +1,70 @@
+var DUNNY_JSON = {
+    "pagination": {
+      "limit": 100,
+      "offset": 0,
+      "count": 1,
+      "total": 1
+    },
+    "data": [
+      {
+        "flight_date": "2024-12-07",
+        "flight_status": "scheduled",
+        "departure": {
+          "airport": "John F Kennedy International",
+          "timezone": "America/New_York",
+          "iata": "JFK",
+          "icao": "KJFK",
+          "terminal": "8",
+          "gate": "2",
+          "delay": 96,
+          "scheduled": "2024-12-07T19:00:00+00:00",
+          "estimated": "2024-12-07T19:00:00+00:00",
+          "actual": "2024-12-07T20:35:00+00:00",
+          "estimated_runway": "2024-12-07T20:35:00+00:00",
+          "actual_runway": "2024-12-07T20:35:00+00:00"
+        },
+        "arrival": {
+          "airport": "Heathrow",
+          "timezone": "Europe/London",
+          "iata": "LHR",
+          "icao": "EGLL",
+          "terminal": "3",
+          "gate": "2",
+          "baggage": null,
+          "delay": 66,
+          "scheduled": "2024-12-08T06:50:00+00:00",
+          "estimated": "2024-12-08T06:50:00+00:00",
+          "actual": null,
+          "estimated_runway": null,
+          "actual_runway": null
+        },
+        "airline": {
+          "name": "American Airlines",
+          "iata": "AA",
+          "icao": "AAL"
+        },
+        "flight": {
+          "number": "100",
+          "iata": "AA100",
+          "icao": "AAL100",
+          "codeshared": null
+        },
+        "aircraft": {
+          "registration": "N762AN",
+          "iata": "B772",
+          "icao": "B772",
+          "icao24": "AA4903"
+        },
+        "live": {
+          "updated": "2024-12-08T08:02:52+00:00",
+          "latitude": 51.4739,
+          "longitude": -0.4624,
+          "altitude": 0,
+          "direction": 233,
+          "speed_horizontal": 1.852,
+          "speed_vertical": 0,
+          "is_ground": true
+        }
+      }
+    ]
+  };
--- airzone/scri.js
+++ airzone/scri.js
@@ -12,24 +12,140 @@ firebase.initializeApp(firebaseConfig);
 const fires = firebase.firestore();
 
 
-  function dwnld() {
-    if (document.getElementById("tic_num").value.trim() != "") {
-        fires
-          .collection("airzone/tickets/flight")
-          .doc(document.getElementById("tic_num").value)
-          .get()
-          .then((doc) => {
-            if (doc.exists) {
-              const data = doc.data();
-              window.open(data.doc);
-              document.getElementById("tic_num").value = "";
-            } else {
-              alert("Your ticket not found on our servers!");
-            }
-          })
-          .catch((error) => {
-            console.error("Error getting document:", error);
-          });
+function dwnld() {
+  if (document.getElementById("tic_num").value.trim() != "") {
+    fires
+      .collection("airzone/tickets/flight")
+      .doc(document.getElementById("tic_num").value)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const data = doc.data();
+          window.open(data.doc);
+          document.getElementById("tic_num").value = "";
+        } else {
+          alert("Your ticket not found on our servers!");
+        }
+      })
+      .catch((error) => {
+        console.error("Error getting document:", error);
+      });
 
-    }
   }
+}
+
+async function fetchData() {
+  const destsref = fires.collection("airzone/destinations/popular");
+  try {
+    const snapshot = await destsref.get();
+    if (snapshot.empty) {
+      console.log("No destinstions.");
+      return;
+    }
+    const dataContainer = document.getElementById("data");
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+      document.getElementById('destn-layout').innerHTML += `
+        <div class="swiper-slide dests">
+          <div class="destination-box gsap-cursor">
+            <div class="destination-img">
+              <img
+                src="${data.doc}"
+                alt="destination image"
+              />
+              <div class="destination-content">
+                <div class="media-left">
+                  <h4 class="box-title">
+                    <a href="https://wa.me/+919400312201?text=${doc.id}">${doc.id}</a>
+                  </h4>
+                </div>
+                <div class="">
+                  <a
+                    href="https://wa.me/+919400312201?text=${doc.id}"
+                    class="th-btn style2 th-icon"
+                    >Book Now</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      `;
+    });
+  } catch (error) {
+    console.error("Error fetching Firestore data:", error);
+  }
+
+  const packageRef = fires.collection("airzone/packages/popular");
+    try {
+      const snapshot = await packageRef.get();
+      if (snapshot.empty) {
+        console.log("No packages.");
+        return;
+      }
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+        document.getElementById('package-layout').innerHTML += `
+              <div class="swiper-slide">
+                <div class="tour-box th-ani gsap-cursor">
+                  <div class="tour-box_img global-img">
+                    <img src="${data.doc}" alt="image" />
+                  </div>
+                  <div class="tour-content">
+                    <h3 class="box-title">
+                      <a href="tour-details.html">${data.name}</a>
+                    </h3>
+                    
+                    <h4 class="tour-box_price">
+                      <span class="currency">₹${data.value}</span>/Person
+                    </h4>
+                    <div class="tour-action">
+                      <a href="contact.html" class="th-btn style4 th-icon"
+                        >Book Now</a
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+          `;
+        });
+      } catch (error) {
+        console.error("Error fetching Firestore data:", error);
+      }
+      const blogRef = fires.collection("airzone/blog/popular");
+      try {
+        const snapshot = await blogRef.get();
+        if (snapshot.empty) {
+          console.log("No blog.");
+          return;
+        }
+
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          document.getElementById('blog-layout').innerHTML += `
+              <div class="swiper-slide">
+                <div class="blog-box th-ani">
+                  <div class="blog-img global-img">
+                    <img src="${data.doc}" alt="blog image" />
+                  </div>
+                  <div class="blog-box_content">
+                    <h3 class="box-title">
+                      <a href="#"
+                        >${data.title}</a
+                      >
+                    </h3>
+                    <p>
+                    ${data.des}
+                    </p>
+                  </div>
+                </div>
+              </div>
+          `;
+        });
+      } catch (error) {
+        console.error("Error fetching Firestore data:", error);
+      }
+}
+
+fetchData();
